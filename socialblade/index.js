108a417af657f13fa9882861c96f7b8a0144d52e
RandomPerson3465 <50644934+RandomPerson3465@users.noreply.github.com>
2024-08-12 01:17:09 -0700

    fix watermark color + add tiktok option

--- socialblade/index.js
+++ socialblade/index.js
@@ -127,14 +127,15 @@ document.getElementById("options.counter.rates.basicMaximum").addEventListener('
 document.getElementById("options.counter.rates.mode.basic.units").addEventListener('input', Interface.setRate)
 document.getElementById("options.counter.rates.mode.basic.baseUnit").addEventListener('change', Interface.setRate)
 document.getElementById("options.counter.rates.mode.basic.baseUnit2").addEventListener('change', Interface.setRate)
-document.getElementById("options-icon").addEventListener("change", () => {
-    switch (document.getElementById("options-icon").value) {
+document.getElementById("options-counter-type").addEventListener("change", () => {
+    switch (document.getElementById("options-counter-type").value) {
         case "youtube":
             document.querySelector(".platform-icon").style.display = "inline";
             document.querySelector(".platform-icon").src = "youtube-icon.png";
             document.getElementById("SearchInput").placeholder = "Enter YouTube Username";
             document.getElementById("realtime-title").innerText = "Real Time YouTube Subscriber Count";
             document.getElementById("live-count-watermark").innerText = "YouTube Live Subscriber Count";
+            document.getElementById("live-count-watermark").style.color = "#d64e33";
             document.getElementById("platform-plus-sign").style.backgroundColor = "#e62117";
             document.getElementById("userimg").style.border = "1px solid #e62117";
             break;
@@ -144,6 +145,7 @@ document.getElementById("options-icon").addEventListener("change", () => {
             document.getElementById("SearchInput").placeholder = "Enter Twitch Username";
             document.getElementById("realtime-title").innerText = "Real Time Twitch Follower Count";
             document.getElementById("live-count-watermark").innerText = "Twitch Live Follower Count";
+            document.getElementById("live-count-watermark").style.color = "#3a0070";
             document.getElementById("platform-plus-sign").style.backgroundColor = "#7a31b3";
             document.getElementById("userimg").style.border = "1px solid #7a31b3";
             break;
@@ -153,14 +155,25 @@ document.getElementById("options-icon").addEventListener("change", () => {
             document.getElementById("SearchInput").placeholder = "Enter Twitter Username";
             document.getElementById("realtime-title").innerText = "Real Time Twitter Follower Count";
             document.getElementById("live-count-watermark").innerText = "Twitter Live Follower Count";
+            document.getElementById("live-count-watermark").style.color = "#003a70";
             document.getElementById("platform-plus-sign").style.backgroundColor = "#317db3";
             document.getElementById("userimg").style.border = "1px solid #317db3";
+            break;
+        case "tiktok":
+            document.querySelector(".platform-icon").style.display = "none";
+            document.getElementById("SearchInput").placeholder = "Enter TikTok Username";
+            document.getElementById("realtime-title").innerText = "Real Time TikTok Follower Count";
+            document.getElementById("live-count-watermark").innerText = "TikTok Live Follower Count";
+            document.getElementById("live-count-watermark").style.color = "#703d00";
+            document.getElementById("platform-plus-sign").style.backgroundColor = "#ff4c74";
+            document.getElementById("userimg").style.border = "1px solid #ff4c74";
             break;
         default:
             document.querySelector(".platform-icon").style.display = "none";
             document.getElementById("SearchInput").placeholder = "Enter Username";
             document.getElementById("realtime-title").innerText = "Real Time Count";
             document.getElementById("live-count-watermark").innerText = "Live Count";
+            document.getElementById("live-count-watermark").style.color = "#703d00";
             document.getElementById("platform-plus-sign").style.backgroundColor = "#333333";
             document.getElementById("userimg").style.border = "1px solid #333333";
     }
